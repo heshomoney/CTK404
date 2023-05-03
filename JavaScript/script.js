c7ccb6416d1ac7d1017192c63171d0ecc5804b88
Esho Oluwaseun <97861438+heshomoney@users.noreply.github.com>
2023-05-03 01:05:55 -0500

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+var newCarousel = document.getElementById('newCarousel');
+
+
+  var customClassStyles = {
+    alignItems: 'center',
+    padding: 'auto',
+    backgroundColor: 'white',
+    'img': {
+      width: '800px',
+      display: 'inline-block'
+    }
+  };
